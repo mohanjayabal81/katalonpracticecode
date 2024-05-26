8c18ee3308020c2228c0b83b05588097a8e5a3d7
mohan-jayabalan <mohanstc@gmail.com>
2024-05-26 10:28:40 +0530

    VisualTesting Checkpoint Commit

---
+++ Test Suites/TS_VisualTestingCheckpoint.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_VisualTestingCheckpoint</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>025d2020-2e0d-42f8-a646-4c90abb3f04c</testSuiteGuid>
+   <testCaseLink>
+      <guid>5246ee0e-72ec-4f90-884f-3864666cf110</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/VisualTestingTestcase</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
